33fe95724ed31740d742787479b4c3d7bd6a22a6
Omar Gastelum <gastelumdev@gmail.com>
2023-05-24 13:27:18 -0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+
+function App() {
+    return (
+        <>
+            <Hero />
+            <Projects />
+        </>
+    );
+}
+
+export default App;
